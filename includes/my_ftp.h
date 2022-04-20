053619b053cf46051a8e1b7d154e9cedc3f36fa6
Thibb1 <thibault.brumaire@gmail.com>
2022-04-20 14:58:59 +0200

    [*] fix l4

--- includes/my_ftp.h
+++ includes/my_ftp.h
@@ -158,8 +158,7 @@ void open_socket(int);
     #define S_PORT S_ADDR.sin_port
 
     #define DEF_OR_ARG(value, ...) value
-    #define ASSERT(v, ...) \
-if (v) { \
+    #define ASSERT(v, ...) if (v) { \
     fprintf(stderr, ERROR, __FILE__, __LINE__, __FUNCTION__); \
     fprintf(stderr, "(%s)\n", DEF_OR_ARG(__VA_ARGS__ __VA_OPT__(,) "Error")); \
     garbage_delete(); \
